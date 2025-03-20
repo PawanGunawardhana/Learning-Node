4c613e0d3126f8e5651b5e02733dfdcf9295cdd6
Pawan Gunawardhana <adpigunawardhana2@gmail.com>
2025-03-20 12:12:41 +0530

    7created express app and redirect,404 page..

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const express = require('express'); //this returns a function and stored in express cont
+const express = require("express"); //this returns a function and stored in express cont
 
 //express app
 const app = express(); //invoke that created function to create an instance of express app
@@ -6,9 +6,22 @@ const app = express(); //invoke that created function to create an instance of e
 //listen for requests
 app.listen(3000); //automatically refers to localhost
 
-app.get('/', (req, res) => {
-    console.log(req.url, req.hostname, req.method);
+app.get("/", (req, res) => {
+  //console.log(req.url, req.hostname, req.method);
+  //res.send('<p>hi</p>');
+  res.sendFile("./views/index.html", { root: __dirname });
+});
 
-    res.send('<p>hi</p>')
+app.get("/about", (req, res) => {
+  res.sendFile("./views/about.html", { root: __dirname });
+});
 
-})
+//redirects
+app.get("/about-me", (req, res) => {
+  res.redirect("/about");
+});
+
+//404
+app.use((req, res) => {
+  res.status(404).sendFile("./views/404.html", { root: __dirname });
+});
