23ff63bda84f7d11f0ec0256b6b8ce83e6e2e9bb
Pawan Gunawardhana <adpigunawardhana2@gmail.com>
2025-03-21 20:17:43 +0530

    some minor ejs dynamic injects added

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 const express = require("express"); //this returns a function and stored in express cont
+const { title } = require("process");
 
 //express app
 const app = express(); //invoke that created function to create an instance of express app
 
 //register view engine
-app.set('view engine', 'ejs');
-
-
+app.set("view engine", "ejs");
 
 //listen for requests
 app.listen(3000); //automatically refers to localhost
@@ -14,26 +13,26 @@ app.listen(3000); //automatically refers to localhost
 app.get("/", (req, res) => {
   //console.log(req.url, req.hostname, req.method);
   //res.send('<p>hi</p>');
-    // res.sendFile("./views/index.html", { root: __dirname });
-    res.render('index',{title : 'Home'});
+  // res.sendFile("./views/index.html", { root: __dirname });
+  res.render("index", { title: "Home" });
 });
 
 app.get("/about", (req, res) => {
-    //res.sendFile("./views/about.html", { root: __dirname });
-    res.render('about');
+  //res.sendFile("./views/about.html", { root: __dirname });
+  res.render("about", { title: "About" });
 });
 
-app.get('/blogs/create', (req, res) => {
-    res.render('create');
-} )
+app.get("/blogs/create", (req, res) => {
+  res.render("create", { title: "Create" });
+});
 
 //redirects
 app.get("/about-me", (req, res) => {
-  res.redirect("/about");
+  res.redirect("/about", { title: "About" });
 });
 
 //404
 app.use((req, res) => {
   //res.status(404).sendFile("./views/404.html", { root: __dirname });
-    res.status(404).render('404')
+  res.status(404).render("404", { title: 404 });
 });
